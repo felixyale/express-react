d79a450e2662208c3a9f29a0f4611cdbc0d2e932
justinyueh <justin.yueh@gmail.com>
2016-06-30 13:44:06 +0800

    router eslint

--- routers/artboard.js
+++ routers/artboard.js
@@ -1,4 +1,5 @@
 var express = require('express');
+/* eslint new-cap: [2, {"capIsNewExceptions": ["express.Router"]}] */
 var router = express.Router();
 var env = process.env.NODE_ENV || 'development';
 
@@ -9,9 +10,7 @@ router.use(function timeLog(req, res, next) {
 });
 
 router.get('/*', function(req, res) {
-  res.render('index', { title: 'Hey', message: 'Hello there!'});
+  res.render('index', { title: 'Hey', message: 'Hello there!' });
 });
-
-
 
 module.exports = router;
