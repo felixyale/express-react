75796d4412c24b93dca8c8ca363df4c4ccb0c873
justinyueh <justin.yueh@gmail.com>
2016-06-30 13:41:57 +0800

    index.js eslint

--- index.js
+++ index.js
@@ -2,24 +2,25 @@ var express = require('express');
 var app = express();
 var env = process.env.NODE_ENV || 'development';
 var router = require('./routers');
+var favicon = require('serve-favicon');
+var server;
 require('./libs/jadeHelper').init(app);
 
-var favicon = require('serve-favicon');
-app.use(favicon(__dirname + '/src/favicon.ico'));
+app.use(favicon(`${__dirname}/src/favicon.ico`));
 
 app.use(express.static('dist'));
 
 app.set('views', './views');
 app.set('view engine', 'jade');
 
-if (env != 'production' && env != 'staging') {
+if (env !== 'production' && env !== 'staging') {
   app.locals.pretty = true;
 }
 
 router(app);
 
-var server = app.listen(3000, function () {
-  var host = server.address().address;
+server = app.listen(3000, function() {
+  // var host = server.address().address;
   var port = server.address().port;
 
   console.log('Example app listening at http://localhost:%s', port);
